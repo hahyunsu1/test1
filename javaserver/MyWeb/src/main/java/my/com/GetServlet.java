c218850f9ff4ccb4f0cc8d233064fb0cbfc26938
hahyunsu <hysdddd@gmail.com>
2022-11-07 13:05:33 +0900

    연습3

--- javaserver/MyWeb/src/main/java/my/com/GetServlet.java
+++ javaserver/MyWeb/src/main/java/my/com/GetServlet.java
@@ -11,13 +11,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/GetTest")
 public class GetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	//html의 form 태그를 통해 전송된 피라미터 같은 req
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html; charset=UTF-8");
 		PrintWriter out=res.getWriter();
+		String name=req.getParameter("name");
+		String user=req.getParameter("userid");
 		out.println("<h1>GetServlet</h1>");
 		out.println("<h2>GET방식의 요청을 처리할 때는 doGet()메서드를 오버라이드 합니다.</h2>");
+		
+		out.println("<h3>이 름 : "+name+"</h3>");
+		out.println("<h3>아이디 : "+user+"</h3>");
 		out.close();
+		
 	}
 
 }
